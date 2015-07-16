c94f8a7b1bc63226b1fe575d83c0404f6494486c
Birol Miguel Knecht <knecht.birol@gmail.com>
2015-07-16 20:57:22 +0200

    PEP8 changes.

--- signal_comparison.py
+++ signal_comparison.py
@@ -7,13 +7,8 @@ import statistics
 
 
 def compare_files(files, save_preprocessed_files):
-    class Sim:
-        def __init__(self, name1, name2):
-            self.name1 = name1
-            self.name2 = name2
-
     characteristics = {}
-    print("processing recordings:");
+    print("processing recordings:")
     i = 0
     for f in files:
         print('\r{} of {} ~ {}%'.format(i,
@@ -26,7 +21,7 @@ def compare_files(files, save_preprocessed_files):
             sound_file.save(files[f].filename[:-4] + "_p.wav", signal)
         characteristics[f] = mfcc.mfcc(signal)
         i += 1
-    print("\r... done" + (" "*30));  # overwrite prev. message with whitespace
+    print("\r... done" + (" "*30))  # overwrite prev. message with whitespace
     metrics = _metrics_of_characteristics(characteristics, files)
     _analyze_metrics_of_correct_result(metrics, files)
     _simple_metrics_comparison(metrics, files)
@@ -44,36 +39,39 @@ def _metrics_of_characteristics(characteristics, files):
     print("comparing files:")
     percent = -1
     comparisons = 0
-    
+
     for f1 in files:
         if percent != _percent(comparisons, len(files)**2):
             percent = _percent(comparisons, len(files)**2)
             print('\r{}%'.format(percent), end='')
-        
+
         characteristics1 = characteristics[f1]
-        metrics[f1][f1] = 0;
+        metrics[f1][f1] = 0
         for f2 in files:
-            characteristics2 = characteristics[f2]
-            metric = mfcc.dynamic_time_warping_metric_sqr(characteristics1,
-                                                          characteristics2)
-            if f1 == f2:
-                assert abs(metric) < 1e-7
-            metrics[f1][f2] = metric
-            metrics[f2][f1] = metric
+            if f2 != f1:
+                characteristics2 = characteristics[f2]
+                metric = mfcc.dynamic_time_warping_metric_sqr(characteristics1,
+                                                              characteristics2)
+                metrics[f1][f2] = metric
+                metrics[f2][f1] = metric
             comparisons += 1
-    print("\r... done");
+    print("\r... done")
     return metrics
 
 
 def _analyze_metrics_of_correct_result(metrics, files):
-    print("/////////////////////////////\ncomparison of expected with actual results:\n")
+    print("/////////////////////////////\ncomparison of " +
+          "expected with actual results:\n")
     unique_texts = set([files[f].text for f in files])
     for t in unique_texts:
         print(t + ": ")
         files_with_text = [files[f] for f in files if files[f].text == t]
-        metrics_with_text = [metrics[str(f1)][str(f2)] for f1 in files_with_text for f2 in files if str(f1) != str(f2)]
+        metrics_with_text = [metrics[str(f1)][str(f2)]
+                             for f1 in files_with_text
+                             for f2 in files if str(f1) != str(f2)]
         print("mean: {}".format(statistics.mean(metrics_with_text)))
-        print("standard deviation: {}".format(statistics.stdev(metrics_with_text)))
+        standard_deviation = statistics.stdev(metrics_with_text)
+        print("standard deviation: {}".format(standard_deviation))
         print("median: {}".format(statistics.median(metrics_with_text)))
         print("minimum: {}".format(min(metrics_with_text)))
         print("maximum: {}".format(max(metrics_with_text)))
@@ -86,7 +84,7 @@ def _simple_metrics_comparison(metrics, files):
     doubt_made_correct_result_worse = 0
     doubted_wrong_result = 0
     doubt_made_wrong_result_better = 0
-    
+
     for f, dists in metrics.items():
         sorted_dists = sorted(dists.items(), key=operator.itemgetter(1))
         most_similar_file, most_similar_metric = sorted_dists[1]    # skip self
@@ -98,43 +96,60 @@ def _simple_metrics_comparison(metrics, files):
         #    print("\t also similar: {} - {}".format(str(also_similar_file),
         #                                            also_similar_metric))
         # print("\n")
-        
+
         # statistics
-        if files[f].text == files[most_similar_file].text:
+        most_similar_text = files[most_similar_file].text
+        also_similar_text = files[also_similar_file].text
+        if files[f].text == most_similar_text:
             assigned_correctly += 1
             if _considered_similar_too(most_similar_metric,
                                        also_similar_metric):
                 doubted_correct_result += 1
-                if files[most_similar_file].text != files[also_similar_file].text:
+                if most_similar_text != also_similar_text:
                     doubt_made_correct_result_worse += 1
         else:
             if _considered_similar_too(most_similar_metric,
                                        also_similar_metric):
                 doubted_wrong_result += 1
-                if files[f].text == files[also_similar_file].text:
+                if files[f].text == also_similar_text:
                     doubt_made_wrong_result_better += 1
-                    
+
     print("\n\nsummary:")
     print("{} ~ {}% correctly associated".format(assigned_correctly,
-                                                 _percent(assigned_correctly, len(metrics))))
+                                                 _percent(assigned_correctly,
+                                                          len(metrics))))
     if assigned_correctly > 0:
+        doubted_correct_percent = _percent(doubted_correct_result,
+                                           assigned_correctly)
         print("{} ~ {}% of which were doubted".format(doubted_correct_result,
-                                                      _percent(doubted_correct_result, assigned_correctly)))
+                                                      doubted_correct_percent))
         if doubted_correct_result > 0:
-            print("{} ~ {}% of which were made worse".format(doubt_made_correct_result_worse,
-                                                             _percent(doubt_made_correct_result_worse, doubted_correct_result)))
-    print("{} ~ {}% of wrong associations doubted".format(doubted_wrong_result,
-                                                          _percent(doubted_wrong_result, len(metrics) - assigned_correctly)))
+            print("{} ~ {}% of which were made worse".format(
+                doubt_made_correct_result_worse,
+                _percent(doubt_made_correct_result_worse,
+                         doubted_correct_result)))
+    print("{} ~ {}% of wrong associations doubted".format(
+        doubted_wrong_result,
+        _percent(doubted_wrong_result,
+                 len(metrics) - assigned_correctly)))
     if doubted_wrong_result > 0:
-        print("{} ~ {}% of which were made better".format(doubt_made_wrong_result_better,
-                                                          _percent(doubt_made_wrong_result_better, doubted_wrong_result)))
+        print("{} ~ {}% of which were made better".format(
+            doubt_made_wrong_result_better,
+            _percent(doubt_made_wrong_result_better, doubted_wrong_result)))
 
 
-def  _considered_similar_too(metric1, metric2):
+def _considered_similar_too(metric1, metric2):
     similarity_distance = abs(metric1 - metric2)
-    return similarity_distance / (metric1 if metric1 < metric2 else metric2) < 0.1
+    denominator = metric1 if metric1 < metric2 else metric2
+    return similarity_distance / denominator < 0.1
 
 
 # Density-based spatial clustering of applications with noise (DBSCAN)
 def _dbscan_metrics_comparison(metrics):
-    dbscan.dbscan(metrics, 1000, 2)
+    user_choice = 1
+    while(user_choice > 0):
+        msg = ("Please give a minimum distance per " +
+               "cluster for the DBSCAN algorithm: ")
+        user_choice = int(input(msg))
+        if user_choice > 0:
+            dbscan.dbscan(metrics, user_choice, 2)
