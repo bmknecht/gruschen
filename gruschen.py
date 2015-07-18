7941dbb1f26f16f843eac812e82c08515b4a31fb
Birol Miguel Knecht <knecht.birol@gmail.com>
2015-07-18 16:07:59 +0200

    Removed __str__ from Signal for clarity.

--- gruschen.py
+++ gruschen.py
@@ -3,6 +3,7 @@ import traceback
 import unittest
 
 from cutoff import CutoffTest
+from metrics import MetricsTest
 from mfcc import MFCCTest
 import signal_comparison
 from sound_file import SoundFileTest
@@ -11,13 +12,13 @@ from sound_file import SoundFileTest
 
 
 class Recording:
-    def __init__(self, print_name, text, filename):
-        self.__print_name = print_name
+    def __init__(self, name, text, filename):
+        self.name = name
         self.text = text
         self.filename = filename
 
     def __str__(self):
-        return self.__print_name
+        raise NotImplementedError
 
 
 def run_tests():
@@ -29,12 +30,10 @@ if __name__ == "__main__":
         path = ""
         if len(sys.argv) > 1:
             path = sys.argv[1]
-        files = {s + str(i): Recording(s + str(i),
-                                       s,
-                                       path + s + str(i) + ".wav")
-                 for i in range(1, 15)
-                 for s in ["yes", "no", "red", "green", "yellow"]}
-        metrics = signal_comparison.compare_files(files, False)
+        files = [Recording(s + str(i), s, path + s + str(i) + ".wav")
+                 for i in range(1, 11)
+                 for s in ["yes", "no", "red", "green", "yellow"]]
+        distances = signal_comparison.compare_files(files, False)
     except Exception as e:
         traceback.print_exc(file=sys.stdout)
         print(repr(e))
