85232395a19dfdd44255340eb6f213435f802d98
Birol Miguel Knecht <knecht.birol@gmail.com>
2015-07-19 20:35:09 +0200

    put common number in variable

--- gruschen/preprocessing/trimming.py
+++ gruschen/preprocessing/trimming.py
@@ -8,16 +8,17 @@ def trim_all_zeros(signal):
 
 
 def smart_trim(signal, cutoffvalue):
+    tolerance = signal.sampleRate/200   # 5 ms
     start = 0
     for i in range(len(signal)):
         if abs(signal[i]) > cutoffvalue:
-            start = max(i-signal.sampleRate/2, 0)
+            start = max(i-tolerance, 0)
             break
 
     end = len(signal) - 1
     for i in reversed(range(len(signal))):
         if abs(signal[i]) > cutoffvalue:
-            end = min(i+signal.sampleRate/2, len(signal))
+            end = min(i+tolerance, len(signal))
             break
 
     return signal[start:end]
