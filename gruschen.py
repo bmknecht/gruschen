e4f59ec3e7f0f7ec64d9e74cd0f462889c0be30f
Murat Knecht <murat.knecht@gmx.de>
2015-07-19 17:32:47 +0800

    Using argparse, to accept input files more flexibly

--- gruschen.py
+++ gruschen.py
@@ -1,14 +1,13 @@
+#!/usr/bin/python3
+
+import argparse
+import glob
+import os
+import re
 import sys
-import traceback
 import unittest
 
-from cutoff import CutoffTest
-from metrics import MetricsTest
-from mfcc import MFCCTest
 import signal_comparison
-from sound_file import SoundFileTest
-
-# TODO:  implement tests for all packages
 
 
 class Recording:
@@ -17,23 +16,69 @@ class Recording:
         self.text = text
         self.filename = filename
 
-    def __str__(self):
-        raise NotImplementedError
+    def __repr__(self):
+        return u"[{}] {} ({})".format(self.text, self.name, self.filename)
 
 
 def run_tests():
     unittest.main()
 
 
-if __name__ == "__main__":
-    try:
-        path = ""
-        if len(sys.argv) > 1:
-            path = sys.argv[1]
-        files = [Recording(s + str(i), s, path + s + str(i) + ".wav")
-                 for i in range(1, 11)
-                 for s in ["yes", "no", "red", "green", "yellow"]]
-        distances = signal_comparison.compare_files(files)
-    except Exception as e:
-        traceback.print_exc(file=sys.stdout)
-        print(repr(e))
+def getargs():
+    parser = argparse.ArgumentParser(usage="""Cluster voice files.""")
+    voice_group = parser.add_mutually_exclusive_group(required=True)
+    voice_group.add_argument(
+        "--voice-dir",
+        help="A directory from which to take all .wav files.",
+        action="append",
+        default=[],
+    )
+    voice_group.add_argument(
+        "--voice-files",
+        help="A list of files to cluster.",
+        nargs="+",
+        default=[],
+    )
+    return parser.parse_args()
+
+
+def to_recordings(directories, files):
+    def _get_subject_word(fpath):
+        name, extension = os.path.splitext(os.path.basename(fpath))
+        match = re.match(r"(\D+)\d+", name)
+        if match:
+            return match.group(1)
+        return name
+
+    recordings = []
+    recordings += [
+        Recording(
+            os.path.basename(fpath),
+            _get_subject_word(fpath),
+            fpath,
+        )
+        for fpath in files
+    ]
+    recordings += [
+        Recording(
+            os.path.basename(fpath),
+            _get_subject_word(fpath),
+            fpath,
+        )
+        for directory in directories
+        for fpath in glob.glob(os.path.join(directory, "*.wav"))
+    ]
+    print(recordings)
+    return recordings
+
+
+def main(args):
+    args = getargs()
+    recordings = to_recordings(args.voice_dir, args.voice_files)
+    distances = signal_comparison.compare_files(recordings)
+    print(distances)
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main(getargs()))
