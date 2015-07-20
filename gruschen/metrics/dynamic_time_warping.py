869854af5e920f7ecccc49cd2142f55ad4c7bcb7
Birol Miguel Knecht <knecht.birol@gmail.com>
2015-07-20 10:30:46 +0200

    Removed unnecessary parameters

--- gruschen/metrics/dynamic_time_warping.py
+++ gruschen/metrics/dynamic_time_warping.py
@@ -12,9 +12,9 @@ def _get_dtw_matrix(s, t):
     dtw = _prepare_dtw_matrix(n, m)
     for i in range(1, n+1):
         row = dtw[i, :]
-        cost = _cost_vector_euclidean(s[i-1] - t, m)
+        cost = _cost_vector_euclidean(s[i-1] - t)
         row[1:] = _row_prediction(cost, dtw[i-1, :])
-        _row_adjustment(row, cost, m)
+        _row_adjustment(row, cost)
     return dtw
 
 
@@ -26,20 +26,24 @@ def _prepare_dtw_matrix(n, m):
     return dtw
 
 
-def _cost_vector_euclidean(diff, m):
-    return np.fromiter((np.inner(d, d) for d in diff), np.float, m)
+def _cost_vector_euclidean(diff):
+    return np.fromiter((np.inner(d, d) for d in diff),
+                       np.float,
+                       len(diff))
 
 
 # alternative to euclidean distance, test later (terrible performance!)
-def _cost_vector_absolute(diff, m):
-    return np.fromiter((np.linalg.norm(d, 1) for d in diff), np.float, m)
+def _cost_vector_absolute(diff):
+    return np.fromiter((np.linalg.norm(d, 1) for d in diff),
+                       np.float,
+                       len(diff))
 
 
 def _row_prediction(cost, prev_row):
     return cost + np.minimum(prev_row[0:-1], prev_row[1:])
 
 
-def _row_adjustment(row, cost, m):
-    for j in range(1, m+1):
+def _row_adjustment(row, cost):
+    for j in range(1, len(row)):
         if row[j-1] + cost[j-1] < row[j]:   # is true 1 out of 5 times
             row[j] = row[j-1] + cost[j-1]
